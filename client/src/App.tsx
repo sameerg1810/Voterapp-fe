1bfe502b8975f73dad981a1134a5bb53ba92640b
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2023-11-07 16:31:20 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UserLogin from "./components/UserLogin";
+import AdminSignup from "./components/AdminSignup";
+import UserSignup from "./components/UserSignUp";
+import UserHome from "./components/UserHome";
+import UserPageDataPrint from "./components/UserPageDataPrint";
+import AdminPage from "./components/AdminPage";
+import ListOfkaryakartha from "./components/ListOfkaryakartha";
+import Askleader from "./components/Askleader";
+import Reportincident from "./components/Report-incident";
+import Reportvoter from "./components/ReportVoter";
+import Allforms from "./components/Allforms";
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<UserLogin />}></Route>
+          <Route path="/home" element={<UserHome />}></Route>
+          <Route path="/print" element={<UserPageDataPrint />}></Route>
+          <Route path="/admin/8899" element={<AdminSignup />}></Route>
+          {/* <Route path='/admin/login' element={<AdminLogin/>}></Route> */}
+          <Route path="/admin/page" element={<AdminPage />}></Route>
+          <Route path="/user/login" element={<UserHome />}></Route>
+          <Route path="user/signup" element={<UserSignup />}></Route>
+          <Route path="/report-incident" element={<Reportincident />}></Route>
+          <Route path="/report-voter" element={<Reportvoter />}></Route>
+          <Route path="/list" element={<ListOfkaryakartha />}></Route>
+          <Route path="/Askleader" element={<Askleader />}></Route>
+          <Route path="/all" element={<Allforms />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
